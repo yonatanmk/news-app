b74778fae1acbf84e6499b9554785e90c96a03f4
Yonatan MK <yonatanmk@gmail.com>
2018-04-08 21:20:07 -0400

    Working Scaffold with Google OAuth and Landing/DashBoard

---
+++ client/src/components/Landing.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Landing = () => (
+  <div style={{ textAlign: 'center' }}>
+    <h1>
+        News App
+    </h1>
+      Get up to date on the biggest stories!
+  </div>
+);
+
+export default Landing;
---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+
+require('./models/user');
+require('./services/passport');
+const keys = require('./config/keys');
+const authRoutes = require('./routes/auth');
+
+mongoose.connect(keys.mongoURI);
+
+const app = express();
+
+app.use(
+	cookieSession({
+		maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
+		keys: [keys.cookieKey],
+	}),
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+authRoutes(app);
+
+if (process.env.NODE_ENV === 'production') {
+  // Express will serve up production assets
+  // like our main.js file, or main.css file!
+  app.use(express.static('client/build'));
+
+  // Express will serve up the index.html file
+  // if it doesn't recognize the route
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
+const PORT = process.env.PORT || 5000; // lets heroku declare which port our app will use, default of 5000
+app.listen(PORT);
