d7225938c33f3d954fd6fee8d1abc69221bf7fb3
Yonatan MK <yonatanmk@gmail.com>
2018-04-09 17:54:23 -0400

    lodash

--- client/src/components/Source.js
+++ client/src/components/Source.js
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import * as actions from '../actions';
@@ -7,8 +8,9 @@ class Source extends Component {
   componentDidMount() {
 		const { match, stories, fetchStories } = this.props;
     const { id } = match.params;
-    const shouldLoadStories = stories &&
-      (stories.length === 0 || stories[0].source.id !== id);
+    const shouldLoadStories = (stories && stories.length === 0) ||
+      _.get(stories, '[0].source.id') !== id;
+
 		if (shouldLoadStories) {
     	fetchStories(id);
 		}
