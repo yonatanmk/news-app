4833b5124b82c1b32725670fb6de90a7cc8eabdc
Yonatan MK <yonatanmk@gmail.com>
2018-04-09 15:31:36 -0400

    fix link styling

--- client/src/components/SourceBox.js
+++ client/src/components/SourceBox.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { Link } from "react-router-dom";
 
 const SourceBox = ({source}) => (
-  <Link to="/source/tsting">
-    <div className="source-box">
-      <h2>{source.name}</h2>
-      <p>{source.description}</p>
-    </div>
-  </Link>
+  <div className="source-box-wrapper">
+    <Link className="source-box-link" to="/source/tsting">
+      <div className="source-box">
+        <h2>{source.name}</h2>
+        <p>{source.description}</p>
+      </div>
+    </Link>
+  </div>
 )
 
 export default SourceBox;
