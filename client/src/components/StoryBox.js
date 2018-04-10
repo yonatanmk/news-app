b7576b3256a2cab2d14931518edc6d7f5cb96b1d
Yonatan MK <yonatanmk@gmail.com>
2018-04-09 20:14:29 -0400

    StoryBox and back button

---
+++ client/src/components/StoryBox.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { getStoryDate } from '../lib/story-utils';
+
+const SourceBox = ({ story }) => (
+  <div className="source-box source-box-wrapper story-box">
+    <h2>{story.title}</h2>
+    {story.urlToImage && <img src={story.urlToImage} alt="Unavailable" width="500" />}
+    <p>{getStoryDate(story)}</p>
+    <p>{story.description} <a href={story.url}>Read More</a></p>
+  </div>
+);
+
+export default SourceBox;
